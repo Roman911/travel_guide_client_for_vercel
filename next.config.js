2eacb0432c4a98d06266bd779265ee212e4af878
Roman911 <roma-lysyk@ukr.net>
2022-09-29 09:30:45 +0300

    init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const serverIP = 'https://tg-server-herocu.herokuapp.com'
 const localhostIP = 'http://localhost:3005'
-const dev = false
+const dev = true
 const IP = dev ? localhostIP : serverIP
 
 module.exports = {
